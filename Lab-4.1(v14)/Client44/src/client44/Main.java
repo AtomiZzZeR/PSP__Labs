5c737552343e902ed8a7d08cf6511ae06249de73
VitaliyD <TheWalkSideMail@gmail.com>
2021-12-25 01:30:40 +0300

    fix: change file paths

--- Lab-4.1(v14)/Client44/src/client44/Main.java
+++ Lab-4.1(v14)/Client44/src/client44/Main.java
@@ -35,7 +35,7 @@ public class Main {
         
         switch(rand) {
             case 1: 
-                FileReader reader1 = new FileReader("C:\\Users\\TheWalkSide\\Documents\\NetBeansProjects\\Client44\\src\\client44\\sonets\\1.txt");
+                FileReader reader1 = new FileReader("sonets/1.txt");
         BufferedReader br = new BufferedReader(reader1);
         String line;
         while ((line = br.readLine()) != null) {
@@ -43,7 +43,7 @@ public class Main {
         }
                 break;
             case 2: 
-                FileReader reader2 = new FileReader("C:\\Users\\TheWalkSide\\Documents\\NetBeansProjects\\Client44\\src\\client44\\sonets\\2.txt");
+                FileReader reader2 = new FileReader("sonets/2.txt");
         BufferedReader br2 = new BufferedReader(reader2);
         String line2;
         while ((line = br2.readLine()) != null) {
@@ -51,7 +51,7 @@ public class Main {
         }
                 break;
             case 3: 
-                FileReader reader3 = new FileReader("C:\\Users\\TheWalkSide\\Documents\\NetBeansProjects\\Client44\\src\\client44\\sonets\\3.txt");
+                FileReader reader3 = new FileReader("sonets/3.txt");
         BufferedReader br3 = new BufferedReader(reader3);
         String line3;
         while ((line = br3.readLine()) != null) {
@@ -59,7 +59,7 @@ public class Main {
         }
                 break;
             case 4: 
-                FileReader reader4 = new FileReader("C:\\Users\\TheWalkSide\\Documents\\NetBeansProjects\\Client44\\src\\client44\\sonets\\4.txt");
+                FileReader reader4 = new FileReader("sonets/4.txt");
         BufferedReader br4 = new BufferedReader(reader4);
         String line4;
         while ((line = br4.readLine()) != null) {
@@ -67,7 +67,7 @@ public class Main {
         }
                 break;
             case 5: 
-                FileReader reader5 = new FileReader("C:\\Users\\TheWalkSide\\Documents\\NetBeansProjects\\Client44\\src\\client44\\sonets\\5.txt");
+                FileReader reader5 = new FileReader("sonets/5.txt");
         BufferedReader br5 = new BufferedReader(reader5);
         String line5;
         while ((line = br5.readLine()) != null) {
@@ -75,7 +75,7 @@ public class Main {
         }
                 break;
             case 6: 
-                FileReader reader6 = new FileReader("C:\\Users\\TheWalkSide\\Documents\\NetBeansProjects\\Client44\\src\\client44\\sonets\\6.txt");
+                FileReader reader6 = new FileReader("sonets/6.txt");
         BufferedReader br6 = new BufferedReader(reader6);
         String line6;
         while ((line = br6.readLine()) != null) {
@@ -83,7 +83,7 @@ public class Main {
         }
                 break;
             case 7: 
-                FileReader reader7 = new FileReader("C:\\Users\\TheWalkSide\\Documents\\NetBeansProjects\\Client44\\src\\client44\\sonets\\7.txt");
+                FileReader reader7 = new FileReader("sonets/7.txt");
         BufferedReader br7 = new BufferedReader(reader7);
         String line7;
         while ((line = br7.readLine()) != null) {
@@ -91,7 +91,7 @@ public class Main {
         }
                 break;
             case 8: 
-                FileReader reader8 = new FileReader("C:\\Users\\TheWalkSide\\Documents\\NetBeansProjects\\Client44\\src\\client44\\sonets\\8.txt");
+                FileReader reader8 = new FileReader("sonets/8.txt");
         BufferedReader br8 = new BufferedReader(reader8);
         String line8;
         while ((line = br8.readLine()) != null) {
@@ -99,7 +99,7 @@ public class Main {
         }
                 break;
             case 9: 
-                FileReader reader9 = new FileReader("C:\\Users\\TheWalkSide\\Documents\\NetBeansProjects\\Client44\\src\\client44\\sonets\\9.txt");
+                FileReader reader9 = new FileReader("sonets/9.txt");
         BufferedReader br9 = new BufferedReader(reader9);
         String line9;
         while ((line = br9.readLine()) != null) {
@@ -107,7 +107,7 @@ public class Main {
         }
                 break;
             case 10: 
-                FileReader reader10 = new FileReader("C:\\Users\\TheWalkSide\\Documents\\NetBeansProjects\\Client44\\src\\client44\\sonets\\10.txt");
+                FileReader reader10 = new FileReader("sonets/10.txt");
         BufferedReader br10 = new BufferedReader(reader10);
         String line10;
         while ((line = br10.readLine()) != null) {
