61e9de36c6955b7c6cb83bb34317f90474f96db5
VitaliyD <TheWalkSideMail@gmail.com>
2021-12-25 11:45:55 +0300

    init: add lab-work-5.4 variant 10

---
+++ Lab-5.4(v10)/src/java/Servlets/OutputServlet.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Servlets;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "OutputServlet", urlPatterns = {"/OutputServlet"})
+public class OutputServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter pw = response.getWriter();
+        
+        String quantity = request.getParameter("Quantity");
+        String size = request.getParameter("Size");   
+        int num = Integer.parseInt(quantity);
+        
+        String src = "C:\\Users\\TheWalkSide\\Documents\\NetBeansProjects\\Lab5ForSlavyan\\src\\java\\Servlets\\text.txt";
+        
+        FileReader reader = new FileReader(src);
+        BufferedReader br = new BufferedReader(reader);
+
+        String line;
+            for (int i = 1; i <= num; i++) {
+                line = br.readLine();
+                if (line != null) {
+                pw.println(line + "<br>");
+            } 
+                else {
+                break;
+            }   
+            }
+            
+        pw.println("<html>");
+        pw.println(String.format("<body style='font-size: %s'>", size));
+        pw.println("</body>");
+        pw.println("</html>");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    
+}
